88c4dc64cd090c9a7fb3da087c93b606d41c4a5d
Lucas Lima <luscaalima81@gmail.com>
2022-08-18 17:06:36 -0300

    sum of two numbers

--- print('Olá Mundo');.py
+++ print('Olá Mundo');.py
@@ -1,1 +1,5 @@
-print('Olá Mundo');
+# print('Olá Mundo');
+number_1 = int(input('Digite seu primeiro número: '));
+number_2 = int(input('Digite seu segundo número: '));
+number_3 = number_1+number_2;
+print(number_3);

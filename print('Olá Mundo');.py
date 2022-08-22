0f563187094a7bba7a4c3feabc3df1669c33432e
Lucas Lima <luscaalima81@gmail.com>
2022-08-22 10:05:41 -0300

    Tarefa 01 de mc102 — Aritmética com Inteiros

--- print('Olá Mundo');.py
+++ print('Olá Mundo');.py
@@ -1,5 +1,9 @@
 # print('Olá Mundo');
 number_1 = int(input('Digite seu primeiro número: '));
 number_2 = int(input('Digite seu segundo número: '));
+# operation= string(input('Digite a operação que deseja realizar: '));
+print('{} + {} = '.format(number_1, number_2))
+print(number_1 + number_2)
+
 number_3 = number_1+number_2;
 print(number_3);

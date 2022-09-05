ee259fc7a4742504bd01e7b06182c0d5e49262a5
Lucas <luscaalima81@gmail.com>
2022-09-05 14:28:08 -0300

    organizing files folders and branch

---
+++ mc102/lab03.py
@@ -0,0 +1,21 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 3 - Ingresso do Cinema
+# Nome: Lucas Lima Pinheiro 
+# RA: 263267
+###################################################
+
+# leitura de dados
+
+
+# valor do ingresso inteiro
+
+
+
+
+# valor a pagar
+
+
+
+# saída do valor do ingresso
+# print('Valor do ingresso: R$', format(ingresso, '.2f').replace('.', ','))

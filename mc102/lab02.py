2feb09b17c4dbeef632d71f6d9533b8015e02f12
Lucas <luscaalima81@gmail.com>
2022-09-05 14:30:46 -0300

    organizing files folders and branch

---
+++ mc102/lab02.py
@@ -0,0 +1,18 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 2 - Rumo a Marte
+# Nome: Lucas Lima Pinheiro 
+# RA: 263267
+###################################################
+
+# Leitura de dados
+
+
+
+
+
+# Cálculo do tempo total gasto por cada espaçonave 
+
+
+
+# Impressão da resposta

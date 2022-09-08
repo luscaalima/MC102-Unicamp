fcf54756d6b10c506f6a12b5ace9ef3f175f34e8
Lucas <luscaalima81@gmail.com>
2022-09-08 18:09:38 -0300

    lab02 started open tests ok

--- mc102/lab02.py
+++ mc102/lab02.py
@@ -1,18 +1,28 @@
 ###################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 2 - Rumo a Marte
-# Nome: Lucas Lima Pinheiro 
+# Nome: Lucas Lima Pinheiro
 # RA: 263267
 ###################################################
 
 # Leitura de dados
+d1 = int(input())
+v1 = int(input())
+t = int(input())
+d2 = int(input())
+v2 = int(input())
 
+t1 = d1/v1
 
+t2 = (d2/v2) + (t*24)
 
+if t1 < t2:
 
+    print(True)
+else:
+    print(False)
 
-# Cálculo do tempo total gasto por cada espaçonave 
-
+# Cálculo do tempo total gasto por cada espaçonave
 
 
 # Impressão da resposta

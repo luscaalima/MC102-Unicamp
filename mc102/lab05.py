f4cd6c9cfab4e84fb8a6381a8414a0b3dd4c43f7
Lucas <luscaalima81@gmail.com>
2022-09-20 16:42:14 -0300

    lab05 Done

--- mc102/lab05.py
+++ mc102/lab05.py
@@ -6,8 +6,6 @@
 ##################################################
 
 # Leitura do valor da hora
-from distutils.log import debug
-
 
 valor_hora_trabalhada = int(input())
 
@@ -22,7 +20,6 @@ hora_extra = 0
 hora_trabalhada_periodo = 0
 
 for dia in range(dias_trabalhados):
-    # for day in range(7):
     periodos_de_trabalho = int(input())
     if periodos_de_trabalho > 0:
         hora_trabalhada_periodo =0
@@ -41,21 +38,19 @@ for dia in range(dias_trabalhados):
         horas_trabalhadas += final - inicio
         valor += (final - inicio) * valor_hora_trabalhada
         if (final - inicio) > 8:
-            print("passou 0")
             hora_extra += (final - inicio) - 8
 
 
-if horas_trabalhadas > 44 and hora_extra < 0:
+if horas_trabalhadas > 44 and dias_trabalhados == 7:
     hora_extra = horas_trabalhadas - 44
     valor = valor + ((valor_hora_trabalhada * 50 / 100) * hora_extra)
 
-
 # Calculo do valor devido ao funcionário
-if hora_extra > 0 and horas_trabalhadas < 44:
+if hora_extra > 0 and  dias_trabalhados != 7:
     valor = valor + ((valor_hora_trabalhada * 50 / 100) * hora_extra)
-
 
 # Impressão da saída
 print("Horas trabalhadas:", horas_trabalhadas)
 print("Horas extras:", hora_extra)
 print("Valor devido: R$ {:0.2f}".format(valor))
+

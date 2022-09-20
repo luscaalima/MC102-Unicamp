26b5625a9efb81105a489f69fed658e56288dc9d
Lucas <luscaalima81@gmail.com>
2022-09-20 08:21:43 -0300

    lab05 structure

---
+++ mc102/lab05.py
@@ -0,0 +1,25 @@
+##################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 5 - Jornada de Trabalho
+# Nome: Lucas Lima Pinheiro 
+# RA:263267
+##################################################
+
+# Leitura do valor da hora
+V = int(input())
+
+# Leitura do numero de dias trabalhados na semana
+D = int(input())
+
+# Leitora e processamento dos periodos de trabalho de cada dia
+
+
+
+# Calculo do valor devido ao funcionário
+
+
+
+# Impressão da saída
+# print("Horas trabalhadas:", horas_trabalhadas)
+# print("Horas extras:", horas_extras)
+# print("Valor devido: R$ {:0.2f}".format(valor))

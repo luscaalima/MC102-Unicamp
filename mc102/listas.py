3d3734b197630871392e03c475839ede4ccf76d1
Lucas <luscaalima81@gmail.com>
2022-09-21 13:16:20 -0300

    python-study

---
+++ mc102/listas.py
@@ -0,0 +1,113 @@
+# n = int(input("Quantos números serão lidos? "))
+# lista = []
+# for i in range(n):
+#     lista.append(int(input()))
+#     x = int(input("Qual o número a procurar? "))
+#     if x in lista:
+#      print(x, "pertence à lista")
+#     else:
+#      print(x, "não pertence à lista")
+     
+## TUPLAS
+
+# n = int(input("Quantos números serão lidos? "))
+# tupla = ()
+
+# for i in range(n): 
+#     x  = int(input("Digite um numero"))
+#     tupla = tupla + tuple([x])
+    
+# print(type(tupla))       
+# print(tupla)    
+    
+    
+# Dada uma lista L de n valores inteiros, escreva um programa que
+# remova todos os números pares da lista.
+# n = int(input("N"))
+# l = list(range(1,n+1))
+# print("lista",l)   
+# print("tamanho lista",len(l))   
+
+# for i in l :
+#    if (i%2) == 0:
+#        l.remove(i)
+
+
+# print("lista com somente números impares",l)   
+# print("tamanho lista" ,len(l)) 
+
+##
+# l = int(input("Entre com o número de linhas: ")) # l = 3
+# c = int(input("Entre com o número de colunas: ")) # c = 4
+# matriz = []
+# for i in range(l):
+#     linha = []
+#     for j in range(c):
+#         linha.append(0)
+#         matriz.append(linha)
+
+# print(matriz[0])
+# [
+#     [0, 0, 0, 0], 
+#     [0, 0, 0, 0], 
+#     [0, 0, 0, 0]
+# ]
+# Acessando Elementos de uma Matriz
+# matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] #3X3
+# print(matriz)
+# pegar o elemento 5
+# print(matriz[2][1])
+
+
+# Exercício 2 - Dimensões de uma Matriz
+def dimensões(M):
+ linhas = len(M)
+ colunas = len(M[0])
+ for i in range(1, linhas):
+    if len(M[i]) != colunas:
+     return ()
+ return (linhas, colunas)
+
+# Exercício 3 - Imprimindo uma Matriz
+def imprime_matriz(M):
+ (linhas, colunas) = dimensões(M)
+ for i in range(linhas):
+    for j in range(colunas):
+     print(M[i][j], end = " ")
+ print()
+
+
+def transporMatriz(matriz):
+    
+   
+    colunas = len(matriz[0])
+   
+    a = []
+    newLine = []
+    colums = []
+    for i in range(len(matriz)):
+        colums.append(matriz[i])
+        for j in range(colums):
+          a.append(colums)        
+    print('colunas',a)
+    # aTrans.append(newLine)   
+    # print(aTrans)
+    
+l = int(input("Entre com o número de linhas: ")) # l = 3
+c = int(input("Entre com o número de colunas: ")) # c = 4
+matriz = []
+linhas = []
+for i in range(l):
+     linhas = []
+     for j in range(c):
+         linha = int(input(""))
+         linhas.append(linha)
+     matriz.append(linhas)
+
+        
+# print(matriz)
+for i in range(len(matriz)) :
+    print(matriz[i])      
+
+
+transporMatriz(matriz)

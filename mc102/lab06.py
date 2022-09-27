f5873907f3c7157338a25e1098554562d1980732
Lucas <luscaalima81@gmail.com>
2022-09-26 22:07:25 -0300

    starting lab06

---
+++ mc102/lab06.py
@@ -0,0 +1,31 @@
+##################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 6 - Torre de Panquecas
+# Nome:Lucas Lima Pinheiro
+# RA:263267
+##################################################
+
+# Leitura da torre de panquecas
+torre = [int(panqueca) for panqueca in input().split(',')]
+torreOrdenada = sorted(torre)
+
+# print('torre',torre)
+# print('torre Ordenada',torreOrdenada)
+if torre == torreOrdenada:
+  print('Torre estavel')
+else:
+    #faz um movimento 
+    m =-1
+    while m != 0:
+        m = int(input())  
+        movimento = torre[m:]
+        movimento.sort(reverse=True)
+        # print('movimento',movimento)
+        # print('-->',torre[:m])
+        torre = torre[:m]+movimento
+        torre.sort()
+        # print(torre)
+   
+    print('Torre instavel') 
+        
+# Impressão da saída

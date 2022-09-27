3b406f1d9ad6ce5b5396999b15ec046b4c65d831
Lucas <luscaalima81@gmail.com>
2022-09-27 08:39:53 -0300

    lab06 adjustments

--- mc102/lab06.py
+++ mc102/lab06.py
@@ -8,25 +8,38 @@
 # Leitura da torre de panquecas
 torre = [int(panqueca) for panqueca in input().split(' ')]
 torreOrdenada = sorted(torre)
+# print('torre ->',torre)
+
+# if torre == torreOrdenada:
+#   print('Torre estavel')
+# else:
+    #faz um movimento 
+while True:
+#   print(m)
+  m = int(input())  
+  if m== 0:
+      break
+  
+  if m >= len(torre) : 
+    torre = list(reversed(torre))
+  
+  
+  
+  
+  
+  
+  movimento = torre[:-m] # OK
+  # print('movimento',movimento)
+  movimento = list(reversed(movimento))
+  # print('movimento reversed',movimento) #OK
+  # print('-->',torre[:m])
+  torre = movimento+torre[:m]
 
 # print('torre',torre)
-# print('torre Ordenada',torreOrdenada)
+# print('torre Ordenada',torreOrdenada)       
 if torre == torreOrdenada:
-  print('Torre estavel')
+ print('Torre estavel')
 else:
-    #faz um movimento 
-    m = -1
-    while m != 0:
-        m = int(input())  
-        movimento = torre[m:]
-        movimento.sort(reverse=False)
-        # print('movimento',movimento)
-        # print('-->',torre[:m])
-        torre = movimento+torre[:m]
-        
-    if torre == torreOrdenada:
-     print('Torre estavel')
-    else:
-     print('Torre instavel') 
+ print('Torre instavel') 
         
 # Impressão da saída

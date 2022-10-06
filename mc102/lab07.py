59eb8f241b7e4f8ea469e7615628774b6893e3ea
Lucas <luscaalima81@gmail.com>
2022-10-06 17:20:32 -0300

    settings lab07

--- mc102/lab07.py
+++ mc102/lab07.py
@@ -19,10 +19,33 @@ for i2 in range(m):
     a= int(input())
     ataque.append(a)
 print(ataque)
-
+vitorias=0
+empates=0
+derrotas=0
 # Processamento da guerra
-
-
+for i in ataque:
+    for j in defesa:   
+      # Ganhei uma batalha
+      if j>i:
+         vitorias =vitorias+1
+      # Empatei uma batalha
+      elif j == i:    
+        empates= empates+1
+     # Perdi uma batalha
+      elif j<i:  
+        derrotas=derrotas+1
+    print('vitorias ->',vitorias)
+    print('empates ->',empates)
+    print('derrotas ->',derrotas)    
+    if vitorias > derrotas:
+        print('ganhou')
+        break     
+        
+        
+        
+        
+        
+            
 # Saída de dados
 # ...
 # print('Derrota')

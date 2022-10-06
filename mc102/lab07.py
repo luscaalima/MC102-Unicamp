4500bbeea078dceccd3231336566e77977da4583
Lucas <luscaalima81@gmail.com>
2022-10-06 13:45:28 -0300

    lab07 starting

---
+++ mc102/lab07.py
@@ -0,0 +1,30 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 7 - Disconnect
+# Nome:Lucas Lima Pinheiro 
+# RA: 263267
+###################################################
+
+# Leitura das tropas de defesa
+n= int(input())
+defesa = []
+for i in range(n):
+    d= int(input())
+    defesa.append(d)
+print(defesa)
+# Leitura das tropas de ataque
+m= int(input())
+ataque = []
+for i2 in range(m):
+    a= int(input())
+    ataque.append(a)
+print(ataque)
+
+# Processamento da guerra
+
+
+# Saída de dados
+# ...
+# print('Derrota')
+# ...
+# print('Vitória posicionando as tropas a partir da posição', posicao)

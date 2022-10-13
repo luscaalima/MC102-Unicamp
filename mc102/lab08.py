5212666bffee136eda655c173d6e209b30660d1c
Lucas <luscaalima81@gmail.com>
2022-10-13 18:08:44 -0300

    lab08 Done

--- mc102/lab08.py
+++ mc102/lab08.py
@@ -17,34 +17,78 @@ chutes =[]
 for i in range(6):
   chute = input()
   chutes.append(chute)
+  if chutes[-1]== palavra:
+    break
 # print('chutes',chutes)
 arrayShow=[]
-index=0
-world=''
+
+ #cHUTE COM A PALAVRA
+flag = None
 for chute in chutes:
-#  print('chute',chute)
- for letter in palavra :
-   if index <= len(palavra):
-    if letter in chute :
-      # print('index',index)
-      # print('chute',chute[index])
-      if letter == chute[index]:
-        world += letter.upper()
+ if chute == palavra:
+   arrayShow.append(chute.upper())
+   flag=True
+   break
+ else :
+  index=0
+  world=''
+  if len(chute)==len(palavra):
+    for letter in  chute :
+      if letter in palavra:
+        if letter == palavra[index]:
+          world += letter.upper()
+        else:
+          world += letter.lower()  
       else:
-        world += letter
-    else:
-     world +='_'
-   index=index+1  
- print('world',world)     
- arrayShow.append(world) 
+        world += '_'
+      index = index + 1  
+  elif len(chute)>len(palavra): 
+    print('-->',chute[:len(palavra)])
+    for letter in  chute[:len(palavra)] :
+      if letter in chute:
+        if letter == chute[index]:
+          world += letter.upper()
+        else:
+          world += letter.lower()  
+      else:
+        world += '_'
+      index = index + 1  
+#len(chute)<len(palavra)
+  else:
+     for letter in  chute :
+      # print(palavra[index],'==',letter)
+      # print('index',index)
+      # print(palavra[index])
+      if letter in palavra:
+        if letter == palavra[index]:
+          world += letter.upper()
+        else:
+          print('letter',letter)
+          world += letter.lower()  
+      else:
+        world += '_'
+      index = index + 1  
+      
+ arrayShow.append(world)
+
+#  print('chute',chute)
+ 
+ 
+#  print('world',world)     
+#  arrayShow.append(world) 
     
-print(arrayShow)
+# print(arrayShow)
      
 # Leitura dos palpites e impressão do resultado para cada palpite 
    
 # Impressão da linha final
 # ...
-# if flag 
-# print("Resposta correta")
-# # ...
-# print("Palavra correta:", palavra)
+for i in arrayShow:
+    print(i)
+    
+if flag :
+ 
+  print("Resposta correta")
+# ...
+else:
+ print("Palavra correta:", palavra)

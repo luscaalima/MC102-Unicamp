87fa2740febe062741a33b74c8ae607ca344acec
Lucas <luscaalima81@gmail.com>
2022-10-13 07:44:06 -0300

    starting lab08

--- mc102/lab08.py
+++ mc102/lab08.py
@@ -7,42 +7,44 @@
 
 # Leitura da palavra
 palavra = input()
+arrayPalavra = []
+for letter in palavra:
+  arrayPalavra.append(letter)
+# print(arrayPalavra)
+
 chutes =[]
 #Monta array chutes
 for i in range(6):
   chute = input()
   chutes.append(chute)
-print('chutes',chutes)
+# print('chutes',chutes)
 arrayShow=[]
-
+index=0
+world=''
 for chute in chutes:
+#  print('chute',chute)
+ for letter in palavra :
+   if index <= len(palavra):
+    if letter in chute :
+      # print('index',index)
+      # print('chute',chute[index])
+      if letter == chute[index]:
+        world += letter.upper()
+      else:
+        world += letter
+    else:
+     world +='_'
+   index=index+1  
+ print('world',world)     
+ arrayShow.append(world) 
     
-    if chute == palavra:
-        arrayShow.append(palavra)
-    else : 
-        palavra=palavra.split()
-        if len(palavra)>len(chute):
-            
-            chute= chute.split()
-            index1=0
-            string=''
-            for letra in chute:
-                
-                if palavra[index1]==chute[index1]:
-                  string+=palavra[index1]
-                else:
-                
-                
-        else:
-        
-  
-  
-
+print(arrayShow)
+     
 # Leitura dos palpites e impressão do resultado para cada palpite 
    
 # Impressão da linha final
 # ...
-if flag 
-print("Resposta correta")
-# ...
-print("Palavra correta:", palavra)
+# if flag 
+# print("Resposta correta")
+# # ...
+# print("Palavra correta:", palavra)

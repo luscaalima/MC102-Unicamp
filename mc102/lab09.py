6306ce50183d6da31b4da8c8600589a575d05dd6
Lucas <luscaalima81@gmail.com>
2022-10-17 05:06:07 -0300

    lab09  structure

---
+++ mc102/lab09.py
@@ -0,0 +1,21 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 9 - Controle de Estoque 2.0
+# Nome: Lucas Lima Pinheiro 
+# RA: 263267
+###################################################
+
+# Leitura de dados
+estoque = {}
+
+# Processamento
+
+#...
+print("Quantidade indisponivel para a venda de " + X + " unidade(s) do produto " + N + ".")
+
+# Impressão da saída
+# ...
+  print("Produto: " + N)
+  print("Quantidade em Estoque: " + E)
+  print("Pedidos de Compra: " + C)
+  print("Pedidos de Venda: " + V)

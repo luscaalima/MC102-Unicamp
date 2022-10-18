b3efec95518985c0a94da1c1eb5dd6b1970acc0b
Lucas <luscaalima81@gmail.com>
2022-10-18 13:00:47 -0300

    lab09 starting

--- mc102/lab09.py
+++ mc102/lab09.py
@@ -8,14 +8,34 @@
 # Leitura de dados
 estoque = {}
 
-# Processamento
+# print(  'entrada',entrada.split(':')[1])
 
-#...
-print("Quantidade indisponivel para a venda de " + X + " unidade(s) do produto " + N + ".")
+
+while True:
+  entrada = input() 
+  nomeProduto = entrada.split(':')[0]
+  quantidadeProduto = int(entrada.split(':')[1])
+  if nomeProduto in estoque['nomeProduto']:
+    pass
+  item = {
+    'nomeProduto': nomeProduto,
+    'quantidadeProduto':quantidadeProduto
+  }
+  print('item',item)
+  # estoque.update(item)
+  print('estoque',estoque)
+# Processamento
+  if quantidadeProduto > 0:
+    #COMPRA
+    print('compra de ',quantidadeProduto,'unidades do produto',nomeProduto )
+  else :
+    print('venda  de ',-quantidadeProduto,'unidades do produto',nomeProduto )
+  #...
+# print("Quantidade indisponivel para a venda de " + X + " unidade(s) do produto " + N + ".")
 
 # Impressão da saída
 # ...
-  print("Produto: " + N)
-  print("Quantidade em Estoque: " + E)
-  print("Pedidos de Compra: " + C)
-  print("Pedidos de Venda: " + V)
+  # print("Produto: " + N)
+  # print("Quantidade em Estoque: " + E)
+  # print("Pedidos de Compra: " + C)
+  # print("Pedidos de Venda: " + V)

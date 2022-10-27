8c8e95b391f8b48de638101329fa595bcfb621fe
Lucas <luscaalima81@gmail.com>
2022-10-27 18:03:43 -0300

    lab09 back

--- mc102/lab09.py
+++ mc102/lab09.py
@@ -6,30 +6,71 @@
 ###################################################
 
 # Leitura de dados
-estoque = {}
+# estoque = {}
+# print('estoque',len(estoque))
 
 # print(  'entrada',entrada.split(':')[1])
 
 
+estoque = {
+'nomeProduto':[
+  'LUCAS'
+  ],
+'quantidadeProduto':[8]  
+}
+  
+# print ('type',type(estoque))
 while True:
   entrada = input() 
-  nomeProduto = entrada.split(':')[0]
-  quantidadeProduto = int(entrada.split(':')[1])
-  if nomeProduto in estoque['nomeProduto']:
-    pass
-  item = {
-    'nomeProduto': nomeProduto,
-    'quantidadeProduto':quantidadeProduto
-  }
-  print('item',item)
-  # estoque.update(item)
-  print('estoque',estoque)
-# Processamento
-  if quantidadeProduto > 0:
-    #COMPRA
-    print('compra de ',quantidadeProduto,'unidades do produto',nomeProduto )
+  if entrada.upper() =="FIM":
+   break
   else :
-    print('venda  de ',-quantidadeProduto,'unidades do produto',nomeProduto )
+    nomeProduto = entrada.split(':')[0]
+    # X
+    quantidadeProduto = int(entrada.split(':')[1])
+    if quantidadeProduto > 0 :
+      print('nomeProduto',estoque['nomeProduto'])
+      print( 'if ',nomeProduto in estoque['nomeProduto'])
+      if nomeProduto in estoque['nomeProduto']:
+        #Altera quantidadeProduto
+        index =estoque['nomeProduto'].index(nomeProduto)
+        estoque['quantidadeProduto'][index]= quantidadeProduto
+      else: 
+        estoque['nomeProduto'].append(nomeProduto)
+        estoque['quantidadeProduto'].append(quantidadeProduto)
+     #  isso indica um  pedido de compra de X unidades do produto N para reposição do estoque
+    else:
+      if nomeProduto in estoque['nomeProduto']:
+        #Altera quantidadeProduto
+        index =estoque['nomeProduto'].index(nomeProduto)
+        estoque['quantidadeProduto'][index]= quantidadeProduto
+      else: 
+        index = estoque['nomeProduto'].index(nomeProduto)
+        if  (-quantidadeProduto) > estoque['quantidadeProduto'][index]:
+          # tirando do estoque mais do que tem ???
+          #"Quantidade indisponivel para a venda de " + quantidadeProduto + " unidade(s) do produto " + nomeProduto + "."
+         print("Quantidade indisponivel para a venda de " + (-quantidadeProduto) + " unidade(s) do produto " + nomeProduto + ".")
+    
+    
+    
+    
+    
+    
+    
+      # caso contrário, isso indica um pedido de venda de X unidades do produto N
+    
+    # print('nomeProduto', nomeProduto)
+    # print('quantidadeProduto', quantidadeProduto)
+    
+ 
+    #  estoque.update( new_estoque)
+  # estoque.update(item) print('estoque',estoque)
+# # Processamento
+#   if quantidadeProduto > 0:
+#     #COMPRA
+#     print('compra de ',quantidadeProduto,'unidades do produto',nomeProduto )
+#   else :
+#     print('venda  de ',-quantidadeProduto,'unidades do produto',nomeProduto )
   #...
 # print("Quantidade indisponivel para a venda de " + X + " unidade(s) do produto " + N + ".")
 

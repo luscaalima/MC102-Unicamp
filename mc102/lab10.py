f20a24878ba07c77ca3df7686e3cbf59ffe8a116
Lucas <luscaalima81@gmail.com>
2022-11-01 15:00:33 -0300

    lab10 starting

---
+++ mc102/lab10.py
@@ -0,0 +1,20 @@
+#####################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 10 - Caça ao Tesouro
+# Nome:Lucas Lima Pinheiro
+# RA:263267
+#####################################################
+
+# Leitura da matriz
+
+n = int(input())
+matriz = [input().split() for _ in range(n)]
+print('n',matriz)
+  
+
+
+# Leitura e processamento dos caminhos
+
+
+
+# Impressão da saída
